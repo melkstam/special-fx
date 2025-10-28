a02a33dbb678609a2c8b5673f916fa76b0370743
Vilhelm Melkstam <vilhelm.melkstam@gmail.com>
2025-10-28 11:34:21 +0100

    Add request ID middleware to app

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import { Hono } from 'hono'
 import { prettyJSON } from 'hono/pretty-json'
+import { requestId } from 'hono/request-id'
 
 const app = new Hono()
 
 app.use(prettyJSON())
+app.use(requestId())
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
