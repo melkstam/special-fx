29096f27826a1f367cb9de93a0cb6310e4a06dd3
Vilhelm Melkstam <vilhelm.melkstam@gmail.com>
2025-10-30 23:03:38 +0100

    Add currency information map and /currencies endpoint

--- src/index.ts
+++ src/index.ts
@@ -8,13 +8,55 @@ import { zValidator } from "./zod-validator";
 // ECB does not fetch EUR since it's the base currency
 const currencyCodeSchema = z.enum([...ecbCurrencyCodeSchema.options, "EUR"]);
 
+interface CurrencyInformation {
+  code: z.infer<typeof currencyCodeSchema>;
+  name: string;
+}
+
+const currencyInformation = {
+  AUD: { code: "AUD", name: "Australian Dollar" },
+  BGN: { code: "BGN", name: "Bulgarian Lev" },
+  BRL: { code: "BRL", name: "Brazilian Real" },
+  CAD: { code: "CAD", name: "Canadian Dollar" },
+  CHF: { code: "CHF", name: "Swiss Franc" },
+  CNY: { code: "CNY", name: "Chinese Yuan" },
+  CZK: { code: "CZK", name: "Czech Koruna" },
+  DKK: { code: "DKK", name: "Danish Krone" },
+  GBP: { code: "GBP", name: "British Pound Sterling" },
+  HKD: { code: "HKD", name: "Hong Kong Dollar" },
+  HUF: { code: "HUF", name: "Hungarian Forint" },
+  IDR: { code: "IDR", name: "Indonesian Rupiah" },
+  ILS: { code: "ILS", name: "Israeli New Shekel" },
+  INR: { code: "INR", name: "Indian Rupee" },
+  ISK: { code: "ISK", name: "Icelandic Króna" },
+  JPY: { code: "JPY", name: "Japanese Yen" },
+  KRW: { code: "KRW", name: "South Korean Won" },
+  MXN: { code: "MXN", name: "Mexican Peso" },
+  MYR: { code: "MYR", name: "Malaysian Ringgit" },
+  NOK: { code: "NOK", name: "Norwegian Krone" },
+  NZD: { code: "NZD", name: "New Zealand Dollar" },
+  PHP: { code: "PHP", name: "Philippine Peso" },
+  PLN: { code: "PLN", name: "Polish Złoty" },
+  RON: { code: "RON", name: "Romanian Leu" },
+  SEK: { code: "SEK", name: "Swedish Krona" },
+  SGD: { code: "SGD", name: "Singapore Dollar" },
+  THB: { code: "THB", name: "Thai Baht" },
+  TRY: { code: "TRY", name: "Turkish Lira" },
+  USD: { code: "USD", name: "United States Dollar" },
+  ZAR: { code: "ZAR", name: "South African Rand" },
+  EUR: { code: "EUR", name: "Euro" },
+} as const satisfies Record<
+  z.infer<typeof currencyCodeSchema>,
+  CurrencyInformation
+>;
+
 const app = new Hono<{ Bindings: Cloudflare.Env }>();
 
 app.use(prettyJSON());
 app.use(requestId());
 
-app.get("/", (c) => {
-  return c.text("Hello Hono!");
+app.get("/currencies", async (c) => {
+  return c.json(currencyInformation);
 });
 
 app.get(
